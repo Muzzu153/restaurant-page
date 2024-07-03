4fb12194eeaf889ee629444ad8080322d59dcbcf
Muzaffer <muzzusyed153@gmail.com>
2024-07-03 16:25:08 +0530

    Modified contactUs page

--- src/pages/contactUs.js
+++ src/pages/contactUs.js
@@ -1,13 +1,14 @@
-import "../css/contactUs.css"
+import "../css/contactUs.css";
 import createHeader from "../modules/header";
-import locationIcon from "../images/location.png"
-import clockIcon from "../images/clock.png"
-import emailIcon from "../images/email.png"
-import phoneIcon from "../images/phone.png"
+import locationIcon from "../images/location.png";
+import timeIcon from "../images/clock.png";
+import emailIcon from "../images/email.png";
+import phoneIcon from "../images/phone.png";
 import senshiImg from "../images/senshi.jpg";
 import laiosImg from "../images/laios.jpg";
 import marcilleImg from "../images/marcille.jpg";
 import chilchuckImg from "../images/chilchuck.jpg";
+import izutsumiImg from "../images/izutsumi.png";
 
 // const createForm = () =>{
 //     const form = document.createElement("form");
@@ -43,24 +44,30 @@ const Staff = () => {
       name: "Chilchuck",
       src: chilchuckImg,
     },
+    {
+      name: "Izutsumi",
+      src: izutsumiImg,
+    },
   ];
 
   const getStaff = () => {
     return staffArray;
   };
 
-  const createStaffList = (arr, div) => {
-    for (let el of arr) {
+  const createStaffList = (div) => {
+    for (let el of staffArray) {
       const staffInfo = document.createElement("div");
       const staffName = document.createElement("div");
       const staffImg = new Image();
 
-      staffInfo.classList.add("staff-info")
+      staffInfo.classList.add("staff-info");
       staffName.classList.add("staff-name");
 
+      // staffInfo.setAttribute("id", `${el.name.}`)
+
       staffName.textContent = el.name;
-      staffImg.src = el.src
-      staffInfo.append(staffImg, staffName );
+      staffImg.src = el.src;
+      staffInfo.append(staffImg, staffName);
 
       div.appendChild(staffInfo);
     }
@@ -71,32 +78,88 @@ const Staff = () => {
 };
 
 const createContactUS = () => {
-  const { getStaff, createStaffList } = Staff();
-  const staffList = getStaff();
+  const { createStaffList } = Staff();
+  // const staffList = getStaff();
 
-  const contactUsDiv = document.createElement("div");
+  const infoArray = [
+    { name: "The ever changing dungeon", icon: locationIcon },
+    { name: "Open 24x7", icon: timeIcon },
+    { name: "(222)-888 5555", icon: phoneIcon },
+    { name: "Message us", icon: emailIcon },
+  ];
+
+  const contactUsDiv = document.createElement("main");
   contactUsDiv.setAttribute("id", "contact-us");
 
-  const location = document.createElement("div");
-  location.textContent = "The ever changing Dungeon"
+  // const locationDiv = document.createElement("div");
+  // locationDiv.setAttribute("id", "location-div");
+  // locationDiv.classList.add("info-div");
+  // const location = document.createElement("div");
+  // location.textContent = "The ever changing Dungeon";
+  // const locationIcon = new Image();
+  // locationIcon.src = locationImg;
+  // locationDiv.append(locationIcon, location);
 
-  const timings = document.createElement("div");
-  timings.textContent = "Open 24x7"
+  // const timingDiv = document.createElement("div");
+  // timingDiv.setAttribute("id", "timing-div");
+  // timingDiv.classList.add("info-div");
+  // const timingIcon = new Image();
+  // timingIcon.src = clockImg;
+  // const timings = document.createElement("div");
+  // timings.textContent = "Open 24x7";
+  // timingDiv.append(timingIcon, timings);
 
-  const contactNumber = document.createElement("div");
-  contactNumber.textContent= "(222)-888 5555"
+  // const contactNbrDiv = document.createElement("div");
+  // contactNbrDiv.setAttribute("id", "contact-nbr-div");
+  // contactNbrDiv.classList.add("info-div");
+  // const contactNbrIcon = new Image();
+  // contactNbrIcon.src = phoneImg;
+  // const contactNumber = document.createElement("div");
+  // contactNumber.textContent = "(222)-888 5555";
+  // contactNbrDiv.append(contactNbrIcon, contactNumber);
 
-  const emailUs = document.createElement("div");
-  emailUs.textContent = "Message Us";
+  // const emailUsDiv = document.createElement("div");
+  // emailUsDiv.setAttribute("id", "email-us-div");
+  // emailUsDiv.classList.add("info-div");
+  // const emailUsIcon = new Image();
+  // emailUsIcon.src = emailImg;
+  // const emailUs = document.createElement("div");
+  // emailUs.textContent = "Message Us";
+  // emailUsDiv.append(emailUsIcon, emailUs);
+
+  const createInfoDiv = (div) => {
+    for (let el of infoArray) {
+      const infoDiv = document.createElement("div");
+      const infoContent = document.createElement("div");
+      const infoIcon = new Image();
+      // const eachElementDiv = document.createElement("div")
+
+      infoIcon.src = el.icon;
+      infoContent.textContent = el.name;
+
+      infoDiv.classList.add("info-div");
+      infoDiv.append(infoIcon, infoContent);
+      div.append(infoDiv);
+    }
+    return div;
+  };
 
   // contactUsDiv.append()
   const staffMembers = document.createElement("div");
-  staffMembers.setAttribute("id", "staff-members")
-  
-  createStaffList(staffList, staffMembers);
-  
-  contactUsDiv.append(location, timings, contactNumber, emailUs, staffMembers)
-  return contactUsDiv
+  staffMembers.setAttribute("id", "staff-members");
+
+  createInfoDiv(contactUsDiv);
+
+  createStaffList(staffMembers);
+
+  contactUsDiv.append(
+    // locationDiv,
+    // timingDiv,
+    // contactNbrDiv,
+    // emailUsDiv,
+    staffMembers
+  );
+  return contactUsDiv;
 };
 
 const loadContactUS = () => {
@@ -105,7 +168,7 @@ const loadContactUS = () => {
 
   wrapper.append(createHeader(), createContactUS());
 
-  return wrapper
+  return wrapper;
 };
 
 export default loadContactUS;
