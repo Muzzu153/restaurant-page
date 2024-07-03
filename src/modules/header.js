2bd314698874af99b1abe914d08f3988c46dd207
Muzaffer <muzzusyed153@gmail.com>
2024-07-03 22:13:52 +0530

    Seperate loadHeader function

--- src/modules/header.js
+++ src/modules/header.js
@@ -1,31 +1,34 @@
-import "../css/header.css"
+import "../css/header.css";
 
 function createHeader() {
-  
-    const headerDiv = document.createElement("header"); // header direct child to warpper containign name and navBar
-    headerDiv.setAttribute("id", "header");
-  
-    const navBar = document.createElement("div"); //navBar containing navigation of home, menu and contactUs
-    navBar.classList.add("navbar");
-  
-    headerDiv.append(navBar);
-  
-    const homeButton = document.createElement("button");
-    const menuButton = document.createElement("button");
-    const contactUsButtton = document.createElement("button");
+  const headerDiv = document.createElement("header"); // header direct child to warpper containign name and navBar
+  headerDiv.setAttribute("id", "header");
 
-    homeButton.setAttribute("id", "home-btn")
-    menuButton.setAttribute("id","menu-btn")
-    contactUsButtton.setAttribute("id","contactUs-btn")
-  
-    homeButton.textContent = "Home";
-    menuButton.textContent = "Menu";
-    contactUsButtton.textContent = "Contact Us";
-  
-    navBar.append(homeButton, menuButton, contactUsButtton);
-  
-    return headerDiv;
-  }
+  const navBar = document.createElement("div"); //navBar containing navigation of home, menu and contactUs
+  navBar.classList.add("navbar");
 
-  export default createHeader;
+  headerDiv.append(navBar);
 
+  const homeButton = document.createElement("button");
+  const menuButton = document.createElement("button");
+  const contactUsButtton = document.createElement("button");
+
+  homeButton.setAttribute("id", "home-btn");
+  menuButton.setAttribute("id", "menu-btn");
+  contactUsButtton.setAttribute("id", "contactUs-btn");
+
+  homeButton.textContent = "Home";
+  menuButton.textContent = "Menu";
+  contactUsButtton.textContent = "Contact Us";
+
+  navBar.append(homeButton, menuButton, contactUsButtton);
+
+  return headerDiv;
+}
+
+const loadHeader = () => {
+  const wrapper = document.getElementById("wrapper");
+  wrapper.append(createHeader());
+};
+
+export default loadHeader;
